2bacc46a3674b7edc39ecaa11255d1feccaa29a9
Tantoony <tantoony.id@gmail.com>
2022-05-21 22:53:54 +0300

    .

---
+++ apps/Moderator/app/Prefix/sesli.js
@@ -0,0 +1,29 @@
+const { DotCommand } = require("../../../../base/utils");
+class Ysay extends DotCommand {
+
+    constructor(client) {
+        super(client, {
+            name: "sesslli",
+            description: "Seste olmayan etiketler",
+            usage: "ysay @etiket/id",
+            examples: ["ysay 674565119161794560"],
+            category: "Düzen",
+            aliases: [],
+            accaptedPerms: ["yt"],
+            cooldown: 10000
+        });
+    }
+
+    async run(client, message, args) {
+        let rol = message.guild.roles.cache.get(args[0]);
+        if (!rol) return await message.reply(`Böyle bir rol bulunmamaktadır.`);
+        const members = rol.members.filter(m = m.presence.status !== "offline").map(m => `<@${m.id}>`);
+        await message.reply(`\`\`\`${rol.name} Rolüne Sahip Olan ${members.length} Kişi Bulunmaktadır \`\`\``);
+        for (let index = 0; index < Math.floor(members.length / 40) + 1; index++) {
+            await message.reply(`BÖLÜM ${index + 1}:` + `${members.slice(index * 40, (index + 1) * 40).join(', ')}`);
+        }
+
+    }
+}
+
+module.exports = Ysay;
