e267f4bf6228560e066918a38b1fff6147575cbd
Tantoony <tantoony.id@gmail.com>
2022-06-01 06:02:05 +0300

    a

--- apps/Moderator/Events/spamblock.js
+++ apps/Moderator/Events/spamblock.js
@@ -10,15 +10,16 @@ class LinkBlock extends ClientEvent {
     async run(message) {
         const client = this.client;
         if (message.guild && (message.guild.id !== client.config.server)) return;
-        let mcache = this.cooldown.get(`${message.author.id}+${message.channel.id}`) || [];
+        let mcache = this.cooldown.get(`${message.author.id}+${message.channel.id}`);
+        if (mcache) mcache = [];
         const peer = {
             cnt: message.content,
             dte: new Date()
         };
         mcache.push(peer);
-        this.cooldown.set(`${message.author.id}+${message.channel.id}`, mcache.filter(log => log.cnt === message.content && Date.now() - log.dte.getTime() < 5000));
+        this.cooldown.set(`${message.author.id}+${message.channel.id}`, mcache.filter(log => require('moment')(log.dte).add(1, "hour").toDate().getTime() > new Date().getTime()));
         const msglogs = this.cooldown.get(`${message.author.id}+${message.channel.id}`);
-        if (msglogs > 3) {
+        if (msglogs.filter(m => m.cnt === message.content).length > 3) {
             client.emit("cmute", message.author.id, client.user.id, `${message.channel.name} kanalında spam.`, 10);
             message.channel.send(`<@${message.author.id}> \`[${message.author.id}]\` sus sürtük.`);
         }
