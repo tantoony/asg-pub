64c6892c1acb975491fa853e94db787ca24a17b2
Tantoony <tantoony.id@gmail.com>
2022-06-01 04:57:04 +0300

    a

--- apps/Moderator/app/Prefix/afk.js
+++ apps/Moderator/app/Prefix/afk.js
@@ -18,18 +18,13 @@ class Afk extends DotCommand {
         if (sebep.includes("@everyone")) return await message.react("🚫");
         if (sebep.includes("@here")) return await message.react("🚫");
         if (sebep.length > 50 || sebep.length == 0) return await message.react("🚫");
-        const system = await client.models.member.findOne({ _id: message.author.id, "afk_data.note": { $ne: null } });
-        if (!system) {
-            await client.models.member.updateOne({ _id: message.author.id }, {
-                $set: {
-                    afk_data: {
-                        note: sebep,
-                        created: new Date(),
-                        inbox: []
-                    }
-                }
-            });
-        }
+        await client.models.member.updateOne({ _id: message.author.id }, {
+            afk_data: {
+                note: sebep,
+                created: new Date(),
+                inbox: []
+            }
+        });
         await message.react("👍");
     }
 }
