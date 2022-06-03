435daa26769b199a22f67f0c81139ac509788c34
Arda <91464589+ktriacxx@users.noreply.github.com>
2022-06-03 08:11:11 +0300

    enter

--- apps/Registry/Events/cmutePass.js
+++ apps/Registry/Events/cmutePass.js
@@ -15,42 +15,33 @@ class GuildMemberUpdate extends ClientEvent {
 		if (cur.guild.id !== client.config.server) return;
 		if (entry.createdTimestamp <= Date.now() - 5000) return;
 		const exeMember = cur.guild.members.cache.get(this.audit.executor.id);
-		let ohal = false;
-		pm2.list((err, list) => {
-			if (err) return;
-			ohal = list.map(item => item.name).filter(item => item.startsWith("CD")).length > 0;
-		});
-		if (!ohal) {
-			let rolex = [];
-			cur.roles.cache.map((r) => r.id).forEach((r) => {
-				client.models.roles.findOne({ meta: { $elemMatch: { _id: r } } }).then((doc) => {
-					rolex.push(doc._id);
-				});
-			});
-			const model = await client.models.member.findOne({ id: cur.user.id });
-			if (!model) {
-				await client.models.member.create({
-					id: cur.user.id,
-					roles: rolex
-				});
-			} else {
-				await client.models.member.updateOne({ id: cur.user.id }, { $set: { roles: rolex } });
-				client.log(`${entry.executor.username} => [${entry.changes[0].key}] ${entry.target.username} : ${entry.changes[0].new[0].name}`, "mngdb");
-			}
-		}
 		const cmutes = await client.models.penalties.find({ userId: cur.user.id, typeOf: "CMUTE"});
         if (cmutes.length > 0 && cmutes.some(cmute => cmute.until.getTime() > new Date().getTime())) {
             const mute = cmutes.find(cmute => cmute.until.getTime() > new Date().getTime())
 			if (mute && !mute.extras.some(extra => extra.subject === "revoke") && !cur.roles.cache.has(this.data.roles["muted"]) && !entry.executor.bot) {
 				if (exeMember.roles.cache.has(this.data.roles["cmute"])) {
-					
+					await client.models.penalties.updateOne({ userId: mentioned.user.id, typeOf: "CMUTE" }, {
+						$push: {
+							extras: {
+								subject: "revoke",
+								data: {
+									executor: message.author.id,
+									date: new Date(),
+									audit: this.audit.id
+								}
+							}
+						}
+					});
+				} else {
+
+
+
 				}
 				await cur.roles.add(this.data.roles["muted"]);
 				if (exeMember.roles.cache.has(this.data.roles["root"])) return;
 				client.handler.emit("jail", exeMember.user.id, this.client.user.id, "* Mute Açma", "Perma", 1);
 			}
         }
-
 	}
 }
 
