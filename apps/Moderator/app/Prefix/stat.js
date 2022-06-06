ddaf521937a19475e3f58b46eaa97aa29c6b74eb
Tantoony <tantoony.id@gmail.com>
2022-06-06 11:09:08 +0300

    a

--- apps/Moderator/app/Prefix/stat.js
+++ apps/Moderator/app/Prefix/stat.js
@@ -20,16 +20,16 @@ class Stat extends PrefixCommand {
         //if (mentioned.user.id !== message.author.id) args = args.slice(1);
         const since = moment(new Date()).subtract(7, "days").toISOString();
         const vData = await client.models.voice.find({ userId: mentioned.user.id, created: { $gt: since } }, { sort: 1 });
-        const pChannels = await client.models.channels.find({ kindOf: "GUILD_CATEGORY" });
         const vChannels = await client.models.channels.find({ kindOf: "GUILD_VOICE" });
-        let records = [];
+        const records = {};
         for (let t = 0; t < vData.length - 1; t++) {
             const vLog = vData[t];
             const nextData = vData[t + 1];
             const diff = moment(nextData.created).diff(vLog.created);
+            if (!records[vLog.channelId]) records[vLog.channelId] = [];
             if (vLog.channelId) {
                 const vCnl_p = vChannels.find(doc => doc.meta.some(m => m._id === vLog.channelId)).parent;
-                const parentData = pChannels.find(doc => doc.meta.some(m => m._id === vCnl_p));
+                const parentData = await client.models.channels.findOne({ meta: { $elemMatch: { _id: vCnl_p } } });
                 const parent = client.guild.channels.cache.get(parentData.meta.pop()._id);
                 const entry = {
                     category: parent ? parent.name : "\`Bilinmiyor\`",
@@ -38,11 +38,9 @@ class Stat extends PrefixCommand {
                     isStreaming: vLog.webcam || vLog.streaming,
                     duration: diff
                 };
-                records.push(entry)
+                records[vLog.channelId].push(entry);
             }
         }
-        const recordz = records;
-        const kanalGrup = require('lodash').groupBy(recordz, "category");
         console.log(kanalGrup);
         /*
         if (mentioned.user.id !== message.author.id) args = args.slice(1);
