12040558d78dd8dcb1e9e21e2fac17eb105646fb
Tantoony <tantoony.id@gmail.com>
2022-06-08 21:29:39 +0300

    a

--- apps/Moderator/app/Prefix/svla.js
+++ apps/Moderator/app/Prefix/svla.js
@@ -34,10 +34,10 @@ class Eval extends PrefixCommand {
 
             const all = await message.reply(clean(evaled), { code: "xl" });
             message.delete();
-            all.delete({ timeout: 5000 });
+            all.delete({ timeout: 5_000 });
         } catch (err) {
             message.reply(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``).then(msg => msg.delete({ timeout: 5000 }));
-            message.delete({ timeout: 1000 });
+            message.delete({ timeout: 10_000 });
         }
 
     }
