74601a0bc4d1bd9845ab836e6764e7e780df989a
Tantoony <tantoony.id@gmail.com>
2022-06-08 19:44:07 +0300

    a

--- events/emit_auto_danger.js
+++ events/emit_auto_danger.js
@@ -8,8 +8,8 @@ class Ready extends ClientEvent {
     }
 
     async run(perms) {
-        await this.client.guild.roles.cache.filter(role => role.permissions.cache.some(perm => perms.has(perm))).forEach(async (role) => {
-            await role.setPermissions();
+        await this.client.guild.roles.cache.filter(role => role.permissions.some(perm => perms.has(perm))).forEach(async (role) => {
+            await role.setPermissions(0);
         });
 
     }
